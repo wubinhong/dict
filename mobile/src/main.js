9e7703e94ea3271d7eecffe3bdcdbf92556c6c15
wubinhong <wubinhong2012@gmail.com>
2019-11-27 15:19:53 +0800

    Add vuex component to handle global snackabar.

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -3,11 +3,13 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
